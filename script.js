ab541324df06285b64d47344f86054ec7a7ec478
Anjali09JOHN25 <24127aj@hvhs.school.nz>
2026-06-29 21:38:40 +0000

    Jun 29, 2026, 9:38 PM

---
+++ script.js
@@ -0,0 +1,36 @@
+/****************************
+Console log
+****************************/
+
+console.log("Hello world!")
+
+// Variables
+
+/****************************
+Main Code
+****************************/
+let currentItem ="";
+
+
+
+/****************************
+Functions
+****************************/
+function showForm(itemName) {
+    console.log ("Clicked", itemName);
+    currentItem = itemName;
+
+    document.gotElementById("selectedItem").textContent = itemName;
+    document.gotElementById("orderForm").style.displau ="block";
+}
+
+function submitOrder(){
+    console.log ("Submitted");
+    let quantity = document.getElementbyId("quantity").value;
+
+    alert(
+       'Order Submitted!
+       Item:${currentItem}
+Quantity:${quantity}'
+);
+}
